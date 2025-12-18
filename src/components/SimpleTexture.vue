29cba3ed33ed6a777c1a1e30c32043dc026bf4e3
alan-wu <alan.wu@auckland.ac.nz>
2025-12-18 16:45:59 +1300

    Comment out unused library.

--- src/components/SimpleTexture.vue
+++ src/components/SimpleTexture.vue
@@ -117,7 +117,7 @@
 /* eslint-disable no-alert, no-console */
 import { markRaw, shallowRef } from 'vue';
 import { ElMessage } from 'element-plus'
-import { LoopSubdivision } from 'three-subdivide';
+//import { LoopSubdivision } from 'three-subdivide';
 import { readNIFTIFromURL } from "./niftiReader.js"
 import { SideBar } from "@abi-software/map-side-bar";
 import "@abi-software/map-side-bar/dist/style.css";
@@ -212,7 +212,6 @@ export default {
       positionalRotation: true,
       needlesInfo: {},
       infoVisible: false,
-      importing: false,
       sidebarTabs: [
         {title: 'Acupoints', id: 1, type: 'acupoints' },
       ],
